760148940dad47110cb0d49ef1d1be52f7bf70ac
Scott Hillman <hillmanov@gmail.com>
2021-12-11 15:45:15 -0700

    Remove duplicate code

--- day11/day11.go
+++ day11/day11.go
@@ -21,48 +21,7 @@ func Part1() Any {
 
 	totalFlashes := 0
 	for steps := 0; steps < 100; steps++ {
-
-		// Increase each E by 1
-		for row := range octopi {
-			for col := range octopi[row] {
-				octopi[row][col].E += 1
-			}
-		}
-
-		// Flash and ripple out until there were no more flashes
-		for {
-			wasFlash := false
-			// Find all that are more than 9
-			for row := range octopi {
-				for col := range octopi[row] {
-					octopus := &octopi[row][col]
-					if octopus.E > 9 && !octopus.Flashed {
-						octopus.Flashed = true
-						totalFlashes++
-						wasFlash = true
-						for _, n := range octopus.Neighbors {
-							if !n.Flashed {
-								n.E += 1
-							}
-						}
-					}
-				}
-			}
-
-			if !wasFlash {
-				break
-			}
-		}
-
-		// Reset all
-		for row := range octopi {
-			for col := range octopi[row] {
-				octopi[row][col].Flashed = false
-				if octopi[row][col].E > 9 {
-					octopi[row][col].E = 0
-				}
-			}
-		}
+		totalFlashes += step(octopi)
 	}
 
 	return totalFlashes
@@ -72,66 +31,70 @@ func Part2() Any {
 	octopi := getInput()
 
 	for steps := 0; ; steps++ {
-		stepFlashes := 0
-
-		// Increase each E by 1
-		for row := range octopi {
-			for col := range octopi[row] {
-				octopi[row][col].E += 1
-			}
-		}
-
-		// Flash and ripple out until there were no more flashes
-		for {
-			wasFlash := false
-			// Find all that are more than 9
-			for row := range octopi {
-				for col := range octopi[row] {
-					octopus := &octopi[row][col]
-					if octopus.E > 9 && !octopus.Flashed {
-						octopus.Flashed = true
-						stepFlashes++
-						wasFlash = true
-						for _, n := range octopus.Neighbors {
-							if !n.Flashed {
-								n.E += 1
-							}
-						}
-					}
-				}
-			}
-
-			if !wasFlash {
-				break
-			}
-		}
-
-		// Reset all
-		for row := range octopi {
-			for col := range octopi[row] {
-				octopi[row][col].Flashed = false
-				if octopi[row][col].E > 9 {
-					octopi[row][col].E = 0
-				}
-			}
-		}
-
-		if stepFlashes == len(octopi)*len(octopi) {
+		if step(octopi) == len(octopi)*len(octopi) {
 			return steps + 1
 		}
 	}
 }
 
-func getInput() [][]octopus {
+func step(octopi [][]*octopus) int {
+	flashes := 0
+
+	// Increase each E by 1
+	for row := range octopi {
+		for col := range octopi[row] {
+			octopi[row][col].E += 1
+		}
+	}
+
+	// Flash and ripple out until there were no more flashes
+	for {
+		wasFlash := false
+		// Find all that are more than 9
+		for row := range octopi {
+			for col := range octopi[row] {
+				octopus := octopi[row][col]
+				if octopus.E > 9 && !octopus.Flashed {
+					octopus.Flashed = true
+					flashes++
+					wasFlash = true
+					for _, n := range octopus.Neighbors {
+						if !n.Flashed {
+							n.E += 1
+						}
+					}
+				}
+			}
+		}
+
+		if !wasFlash {
+			break
+		}
+	}
+
+	// Reset all
+	for row := range octopi {
+		for col := range octopi[row] {
+			octopi[row][col].Flashed = false
+			if octopi[row][col].E > 9 {
+				octopi[row][col].E = 0
+			}
+		}
+	}
+
+	return flashes
+}
+
+func getInput() [][]*octopus {
 	lines, _ := ReadLines(f, "input.txt")
 
-	octopi := [][]octopus{}
+	octopi := [][]*octopus{}
 
 	for _, line := range lines {
-		rowOctopi := []octopus{}
+		rowOctopi := []*octopus{}
 		for _, c := range line {
 			e, _ := strconv.Atoi(string(c))
-			rowOctopi = append(rowOctopi, octopus{E: e, Neighbors: []*octopus{}})
+			rowOctopi = append(rowOctopi, &octopus{E: e, Neighbors: []*octopus{}})
 		}
 		octopi = append(octopi, rowOctopi)
 	}
@@ -140,35 +103,35 @@ func getInput() [][]octopus {
 		for col := range octopi[row] {
 			// T
 			if row > 0 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row-1][col])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row-1][col])
 			}
 			// TR
 			if row > 0 && col < len(octopi[row])-1 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row-1][col+1])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row-1][col+1])
 			}
 			// R
 			if col < len(octopi[row])-1 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row][col+1])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row][col+1])
 			}
 			//BR
 			if row < len(octopi)-1 && col < len(octopi[row])-1 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row+1][col+1])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row+1][col+1])
 			}
 			//B
 			if row < len(octopi)-1 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row+1][col])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row+1][col])
 			}
 			//BL
 			if row < len(octopi)-1 && col > 0 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row+1][col-1])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row+1][col-1])
 			}
 			// L
 			if col > 0 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row][col-1])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row][col-1])
 			}
 			// TL
 			if row > 0 && col > 0 {
-				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, &octopi[row-1][col-1])
+				octopi[row][col].Neighbors = append(octopi[row][col].Neighbors, octopi[row-1][col-1])
 			}
 		}
 	}
