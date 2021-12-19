f56ea57a0f26880cf224198182d3ec64d5dcaba4
Scott Hillman <hillmanov@gmail.com>
2021-12-18 18:59:11 -0700

    Day 18 complete

--- day18/day18.go
+++ day18/day18.go
@@ -3,7 +3,9 @@ package main
 import (
 	. "adventofcode/utils"
 	"embed"
+	"encoding/json"
 	"fmt"
+	"math"
 )
 
 //go:embed input.txt
@@ -11,41 +13,95 @@ var f embed.FS
 
 type Expression []interface{}
 
-func (e *Expression) Reduce() {
+func (e *Expression) Reduce() *Expression {
+	nested := 0
+	performedAction := false
 
+reducer:
+	for {
+		nested = 0
+		performedAction = false
+
+		for i := 0; i < len(*e); i++ {
+			switch {
+			case nested >= 4 && e.IsMatchAtIndex(i, "["):
+				e.Explode(i + 1) // This is the int inside the bracket
+				performedAction = true
+				goto reducer
+			case e.IsMatchAtIndex(i, "["):
+				nested++
+			case e.IsMatchAtIndex(i, "]"):
+				nested--
+			}
+		}
+
+		for i := 0; i < len(*e); i++ {
+			switch {
+			case e.IsIntAtIndex(i) && e.IntValueAtIndex(i) >= 10:
+				e.Split(i)
+				performedAction = true
+				goto reducer
+			}
+		}
+
+		if !performedAction {
+			break
+		}
+	}
+	return e
 }
 
-func (e *Expression) Explode(index int) {
+func (e *Expression) Add(addE Expression) *Expression {
+	newE := Expression{}
+	newE = append(newE, "[")
+	newE = append(newE, (*e)...)
+	newE = append(newE, ",")
+	newE = append(newE, addE...)
+	newE = append(newE, "]")
+	(*e) = newE
+	return e
+}
 
+func (e *Expression) Explode(indexOfLeftValue int) {
+	indexOfRightValue := e.IndexOfIntRightOfIndex(indexOfLeftValue)
+
+	leftValue := e.IntValueAtIndex(indexOfLeftValue)
+	rightValue := e.IntValueAtIndex(indexOfRightValue)
+
+	e.AddToIntLeftOfIndex(indexOfLeftValue, leftValue)
+	e.AddToIntRightOfIndex(indexOfRightValue, rightValue)
+
+	leftBraceIndex := e.IndexOfOpeningBracketLeftOfIndex(indexOfLeftValue)
+	rightBraceIndex := e.IndexOfClosingBracketRightOfIndex(indexOfRightValue)
+
+	(*e) = append((*e)[:leftBraceIndex], (*e)[rightBraceIndex:]...)
+	(*e)[leftBraceIndex] = 0
 }
 
 func (e *Expression) Split(index int) {
+	value := e.IntValueAtIndex(index)
 
+	leftValue := int(math.Floor(float64(value) / 2))
+	rightValue := int(math.Ceil(float64(value) / 2))
+
+	(*e) = append((*e)[:index], append(Expression{"[", leftValue, ",", rightValue, "]"}, (*e)[index+1:]...)...)
 }
 
 func (e *Expression) AddToIntLeftOfIndex(index int, value int) {
-	i := e.IndexOfIntLeftOfIndex(index)
-	v, ok := (*e)[i].(int)
-	if !ok {
-		panic("not an int at index " + string(i))
+	if i := e.IndexOfIntLeftOfIndex(index); i > 0 {
+		(*e)[i] = int(value + e.IntValueAtIndex(i))
 	}
-	v += value
-	(*e)[i] = v
 }
 
 func (e *Expression) AddToIntRightOfIndex(index int, value int) {
-	i := e.IndexOfIntRightOfIndex(index)
-	v, ok := (*e)[i].(int)
-	if !ok {
-		panic("not an int at index " + string(i))
+	if i := e.IndexOfIntRightOfIndex(index); i > 0 {
+		(*e)[i] = int(value + e.IntValueAtIndex(i))
 	}
-	v += value
-	(*e)[i] = v
 }
 
 func (e *Expression) IndexOfIntRightOfIndex(index int) int {
 	for i := index + 1; i < len(*e); i++ {
-		if _, ok := (*e)[i].(int); ok {
+		if e.IsIntAtIndex(i) {
 			return i
 		}
 	}
@@ -54,7 +110,7 @@ func (e *Expression) IndexOfIntRightOfIndex(index int) int {
 
 func (e *Expression) IndexOfIntLeftOfIndex(index int) int {
 	for i := index - 1; i >= 0; i-- {
-		if _, ok := (*e)[i].(int); ok {
+		if e.IsIntAtIndex(i) {
 			return i
 		}
 	}
@@ -63,7 +119,7 @@ func (e *Expression) IndexOfIntLeftOfIndex(index int) int {
 
 func (e *Expression) IndexOfClosingBracketRightOfIndex(index int) int {
 	for i := index; i < len(*e); i++ {
-		if c, ok := (*e)[i].(string); ok && c == "]" {
+		if e.IsMatchAtIndex(i, "]") {
 			return i
 		}
 	}
@@ -72,39 +128,95 @@ func (e *Expression) IndexOfClosingBracketRightOfIndex(index int) int {
 
 func (e *Expression) IndexOfOpeningBracketLeftOfIndex(index int) int {
 	for i := index; i >= 0; i-- {
-		if c, ok := (*e)[i].(string); ok && c == "]" {
+		if e.IsMatchAtIndex(i, "[") {
 			return i
 		}
 	}
 	return -1
 }
 
-func (e *Expression) ClearSurroundingBracketsAtIndex(index int) {
-	lIndex, rIndex := e.IndexOfOpeningBracketLeftOfIndex(index), e.IndexOfClosingBracketRightOfIndex(index)
-	if lIndex > 0 {
-		(*e)[lIndex] = ""
+func (e *Expression) IntValueAtIndex(index int) int {
+	if v, ok := (*e)[index].(int); ok {
+		return v
 	}
-	if rIndex > 0 {
-		(*e)[rIndex] = ""
+	panic("Not an int at index")
+}
+
+func (e *Expression) IsIntAtIndex(index int) bool {
+	_, ok := (*e)[index].(int)
+	return ok
+}
+
+func (e *Expression) IsMatchAtIndex(index int, c string) bool {
+	v, ok := (*e)[index].(string)
+	return ok && v == c
+}
+
+func (e *Expression) String() string {
+	s := ""
+	for i := 0; i < len(*e); i++ {
+		s += fmt.Sprintf("%+v", (*e)[i])
 	}
+	return s
+}
+
+func (e Expression) Materialize() []interface{} {
+	var materialized []interface{}
+	json.Unmarshal([]byte(e.String()), &materialized)
+	return materialized
 }
 
 func Part1() Any {
 	expressions := getInput()
-	fmt.Printf("expressions = %+v\n", expressions)
 
-	e := expressions[0]
-	i := e.IndexOfIntRightOfIndex(0)
+	runningExpression := expressions[0]
+	for _, e := range expressions[1:] {
+		runningExpression.Add(e)
+		runningExpression.Reduce()
+	}
+	runningExpression.Reduce()
 
-	e.AddToIntRightOfIndex(i, 5)
+	mag := getMagnitude(runningExpression.Materialize())
 
-	fmt.Printf("e = %+v\n", e)
-
-	return nil
+	return mag
 }
 
 func Part2() Any {
-	return nil
+	expressions := getInput()
+
+	maxMag := 0
+	for i := 0; i < len(expressions)-1; i++ {
+		for j := i + 1; j < len(expressions)-1; j++ {
+			if i == j {
+				continue
+			}
+
+			// Make copies so we don't mangle everything as we reduce
+			a := expressions[i][:]
+			b := expressions[j][:]
+
+			c := expressions[j][:]
+			d := expressions[i][:]
+
+			maxMag = MaxInt(
+				maxMag,
+				MaxInt(
+					getMagnitude(a.Add(b).Reduce().Materialize()),
+					getMagnitude(c.Add(d).Reduce().Materialize()),
+				),
+			)
+		}
+	}
+	return maxMag
+}
+
+func getMagnitude(e interface{}) int {
+	if v, ok := e.(float64); ok {
+		return int(v)
+	}
+
+	ee, _ := e.([]interface{})
+	return 3*getMagnitude(ee[0]) + 2*getMagnitude(ee[1])
 }
 
 func getInput() []Expression {
