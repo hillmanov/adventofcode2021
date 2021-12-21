f5943d68418d991534f53168deb8c64f17e3c2fd
Scott Hillman <hillmanov@gmail.com>
2021-12-21 16:11:51 -0700

    Day 21 part 1

---
+++ day21/day21.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	. "adventofcode/utils"
+	"embed"
+	"fmt"
+)
+
+//go:embed input.txt
+var f embed.FS
+
+type Dice int
+
+func (d *Dice) Roll(times int) int {
+	sum := int(*d)
+	for i := 0; i < times-1; i++ {
+		(*d)++
+		if *d > 100 {
+			*d = 1
+		}
+		sum += int(*d)
+	}
+	(*d)++
+	if *d > 100 {
+		*d = 1
+	}
+	return sum
+}
+
+type PlayerPosition int
+
+func (pp *PlayerPosition) Move(times int) int {
+	for i := 0; i < times; i++ {
+		*pp++
+		if *pp > 10 {
+			*pp = 1
+		}
+	}
+	return int(*pp)
+}
+
+func Part1() Any {
+	player1Position, player2Position := getInput()
+	player1Score, player2Score := 0, 0
+
+	diceRolls := 0
+	dice := Dice(1)
+	for {
+		// Player 1
+		move1 := dice.Roll(3)
+		diceRolls += 3
+		player1Position.Move(move1)
+		player1Score += int(player1Position)
+
+		if player1Score >= 1000 {
+			return player2Score * diceRolls
+		}
+
+		// Player 2
+		move2 := dice.Roll(3)
+		diceRolls += 3
+		player2Position.Move(move2)
+		player2Score += int(player2Position)
+
+		if player2Score >= 1000 {
+			return player1Score * diceRolls
+		}
+	}
+}
+
+func Part2() Any {
+	return nil
+}
+
+func increment(currentValue *int, max int) int {
+	*currentValue = *currentValue + 1
+	if *currentValue > max {
+		*currentValue = 1
+	}
+	return *currentValue
+}
+
+func getInput() (player1Start, player2Start PlayerPosition) {
+	lines, _ := ReadLines(f, "input.txt")
+
+	fmt.Sscanf(lines[0], "Player 1 starting position: %d", &player1Start)
+	fmt.Sscanf(lines[1], "Player 2 starting position: %d", &player2Start)
+
+	return player1Start, player2Start
+}
+
+func main() {
+	part1Solution := Part1()
+	part2Solution := Part2()
+
+	fmt.Printf("Day 21: Part 1: = %+v\n", part1Solution)
+	fmt.Printf("Day 21: Part 2: = %+v\n", part2Solution)
+}
---
+++ day21/go.mod
@@ -0,0 +1,7 @@
+module adventofcode/day21
+
+go 1.17
+
+replace adventofcode/utils => ../utils
+
+require adventofcode/utils v0.0.0
