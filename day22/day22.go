4dac007dcdc702bfb6cd8b16ec813e66363103ae
Scott Hillman <hillmanov@gmail.com>
2021-12-23 05:18:14 -0700

    Day 22.2 progress

--- day22/day22.go
+++ day22/day22.go
@@ -9,35 +9,66 @@ import (
 //go:embed input.txt
 var f embed.FS
 
-type Step struct {
-	State  string
-	StartX int
-	EndX   int
-	StartY int
-	EndY   int
-	StartZ int
-	EndZ   int
+type Cuboid struct {
+	State        string
+	MinX         int
+	MaxX         int
+	MinY         int
+	MaxY         int
+	MinZ         int
+	MaxZ         int
+	Intersectors []Cuboid
+	Absorbed     bool
 }
 
-type Cube struct {
+type Voxel struct {
 	X int
 	Y int
 	Z int
 }
 
-func (s Step) VolumeOfCuboid() int {
-	return (s.EndX - s.StartX) * (s.EndY - s.StartY) * (s.EndZ - s.StartZ)
+func (c Cuboid) Volume() int {
+	return (c.MaxX - c.MinX) * (c.MaxY - c.MinY) * (c.MaxZ - c.MinZ)
 }
 
-func (A Step) IntersectionVolume(B Step) int {
-	return MaxInt(MinInt(B.EndX, A.EndX)-MaxInt(B.StartX, A.StartX), 0) *
-		MaxInt(MinInt(B.EndY, A.EndY)-MaxInt(B.StartY, A.StartY), 0) *
-		MaxInt(MinInt(B.EndZ, A.EndZ)-MaxInt(B.StartZ, A.StartZ), 0)
+func (c Cuboid) IsNil() bool {
+	return c.Volume() == 0
+}
+
+func (A Cuboid) Intersects(B Cuboid) bool {
+	return (MinInt(B.MaxX, A.MaxX)-MaxInt(B.MinX, A.MinX) > 0) &&
+		(MinInt(B.MaxY, A.MaxY)-MaxInt(B.MinY, A.MinY) > 0) &&
+		(MinInt(B.MaxZ, A.MaxZ)-MaxInt(B.MinZ, A.MinZ) > 0)
+}
+
+func (A Cuboid) Intersection(B Cuboid) Cuboid {
+	if A.Intersects(B) {
+		return Cuboid{
+			MinX: MaxInt(B.MinX, A.MinX),
+			MaxX: MinInt(B.MaxX, A.MaxX),
+			MinY: MaxInt(B.MinY, A.MinY),
+			MaxY: MinInt(B.MinY, A.MinY),
+			MinZ: MaxInt(B.MinZ, A.MinZ),
+			MaxZ: MinInt(B.MinZ, A.MinZ),
+		}
+	}
+
+	return Cuboid{}
+}
+
+func (A Cuboid) Split(B Cuboid) []Cuboid {
+	splits := []Cuboid{}
+
+	intersection := A.Intersection(B)
+
+	// Check each plane to see if what splits we need
+
+	return splits
 }
 
 type State = string
 
-type Core map[Cube]State
+type Core map[Voxel]State
 
 func Part1() Any {
 	steps := getInput()
@@ -45,18 +76,18 @@ func Part1() Any {
 	core := Core{}
 
 	for _, step := range steps {
-		if (step.StartX < -50 && step.EndX < 50) || (step.StartY < -50 && step.EndY < 50) || (step.StartZ < -50 && step.EndZ < 50) {
+		if (step.MinX < -50 && step.MaxX < 50) || (step.MinY < -50 && step.MaxY < 50) || (step.MinZ < -50 && step.MaxZ < 50) {
 			continue
 		}
 
-		if (step.StartX > 50 && step.EndX > 50) || (step.StartY > -50 && step.EndY > 50) || (step.StartZ > -50 && step.EndZ > 50) {
+		if (step.MinX > 50 && step.MaxX > 50) || (step.MinY > -50 && step.MaxY > 50) || (step.MinZ > -50 && step.MaxZ > 50) {
 			continue
 		}
 
-		for x := step.StartX; x <= step.EndX; x++ {
-			for y := step.StartY; y <= step.EndY; y++ {
-				for z := step.StartZ; z <= step.EndZ; z++ {
-					core[Cube{x, y, z}] = step.State
+		for x := step.MinX; x <= step.MaxX; x++ {
+			for y := step.MinY; y <= step.MaxY; y++ {
+				for z := step.MinZ; z <= step.MaxZ; z++ {
+					core[Voxel{x, y, z}] = step.State
 				}
 			}
 		}
@@ -66,7 +97,7 @@ func Part1() Any {
 	for x := -50; x <= 50; x++ {
 		for y := -50; y <= 50; y++ {
 			for z := -50; z <= 50; z++ {
-				if v := core[Cube{x, y, z}]; v == "on" {
+				if v := core[Voxel{x, y, z}]; v == "on" {
 					countOn++
 				}
 			}
@@ -77,26 +108,30 @@ func Part1() Any {
 }
 
 func Part2() Any {
-	step := Step{
-		StartX: 0,
-		EndX:   5,
-		StartY: 0,
-		EndY:   5,
-		StartZ: 0,
-		EndZ:   5,
-	}
+	cuboids := getInput()
 
-	return step.VolumeOfCuboid()
+	// For each cuboid in the list:
+	// Check if there is an intersection:
+	// If both boxes are ON
+	// aCuboid stays the SAME
+	// bCuboid SPLITS into up to 3 different sub-cuiboids
+	//
+	// If aCuboid is ON and bCuboid is OFF
+	// aCuboid splits into 0-3 sub cubes
+
+	return nil
 }
 
-func getInput() []Step {
+func getInput() []Cuboid {
 	lines, _ := ReadLines(f, "input.txt")
 
-	steps := []Step{}
+	steps := []Cuboid{}
 
 	for _, line := range lines {
-		step := Step{}
-		fmt.Sscanf(line, "%s x=%d..%d,y=%d..%d,z=%d..%d", &step.State, &step.StartX, &step.EndX, &step.StartY, &step.EndY, &step.StartZ, &step.EndZ)
+		step := Cuboid{
+			Intersectors: []Cuboid{},
+		}
+		fmt.Sscanf(line, "%s x=%d..%d,y=%d..%d,z=%d..%d", &step.State, &step.MinX, &step.MaxX, &step.MinY, &step.MaxY, &step.MinZ, &step.MaxZ)
 		steps = append(steps, step)
 	}
 
@@ -104,9 +139,9 @@ func getInput() []Step {
 }
 
 func main() {
-	part1Solution := Part1()
+	// part1Solution := Part1()
 	part2Solution := Part2()
 
-	fmt.Printf("Day 22: Part 1: = %+v\n", part1Solution)
+	// fmt.Printf("Day 22: Part 1: = %+v\n", part1Solution)
 	fmt.Printf("Day 22: Part 2: = %+v\n", part2Solution)
 }
