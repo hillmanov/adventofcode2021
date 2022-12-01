49ab525332e1a455517c3d4cebfbad7f6298f316
Scott Hillman <hillmanov@gmail.com>
2022-11-30 18:16:21 -0700

    Added an A* algorithm to day 23 to help

--- day23/day23.go
+++ day23/day23.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	. "adventofcode/utils"
+	"container/heap"
 	"embed"
 	"fmt"
 )
@@ -21,6 +22,92 @@ func main() {
 	part1Solution := Part1()
 	part2Solution := Part2()
 
-  fmt.Printf("Day 23: Part 1: = %+v\n", part1Solution)
+	fmt.Printf("Day 23: Part 1: = %+v\n", part1Solution)
 	fmt.Printf("Day 23: Part 2: = %+v\n", part2Solution)
+}
+
+// Code lifted and modified from this implementation: https://github.com/beefsack/go-astar
+type Pather interface {
+	// PathNeighbors returns the direct neighboring nodes of this node which can be pathed to.
+	PathNeighbors() []Pather
+	// PathNeighborCost calculates the exact movement cost to neighbor nodes.
+	PathNeighborCost(to Pather) float64
+	// PathEstimatedCost is a heuristic method for estimating movement costs between non-adjacent nodes.
+	PathEstimatedCost(to Pather) float64
+}
+
+// node is a wrapper to store A* data for a Pather node.
+type node struct {
+	pather Pather
+	cost   float64
+	rank   float64
+	parent *node
+	open   bool
+	closed bool
+	index  int
+}
+
+// nodeMap is a collection of nodes keyed by Pather nodes for quick reference.
+type nodeMap map[Pather]*node
+
+// get gets the Pather object wrapped in a node, instantiating if required.
+func (nm nodeMap) get(p Pather) *node {
+	n, ok := nm[p]
+	if !ok {
+		n = &node{
+			pather: p,
+		}
+		nm[p] = n
+	}
+	return n
+}
+
+// Path calculates a short path and the distance between the two Pather nodes.
+//
+// If no path is found, found will be false.
+func Path(from, to Pather) (path []Pather, distance float64, found bool) {
+	nm := nodeMap{}
+	nq := &priorityQueue{}
+	heap.Init(nq)
+	fromNode := nm.get(from)
+	fromNode.open = true
+	heap.Push(nq, fromNode)
+	for {
+		if nq.Len() == 0 {
+			return
+		}
+		current := heap.Pop(nq).(*node)
+		current.open = false
+		current.closed = true
+
+		if current == nm.get(to) {
+			// Found a path to the goal.
+			p := []Pather{}
+			curr := current
+			for curr != nil {
+				p = append(p, curr.pather)
+				curr = curr.parent
+			}
+			return p, current.cost, true
+		}
+
+		for _, neighbor := range current.pather.PathNeighbors() {
+			cost := current.cost + current.pather.PathNeighborCost(neighbor)
+			neighborNode := nm.get(neighbor)
+			if cost < neighborNode.cost {
+				if neighborNode.open {
+					heap.Remove(nq, neighborNode.index)
+				}
+				neighborNode.open = false
+				neighborNode.closed = false
+			}
+			if !neighborNode.open && !neighborNode.closed {
+				neighborNode.cost = cost
+				neighborNode.open = true
+				neighborNode.rank = cost + neighbor.PathEstimatedCost(to)
+				neighborNode.parent = current
+				heap.Push(nq, neighborNode)
+			}
+		}
+	}
 }
